124a09bdb40053c10e42acead668227863f66e8c
Crescent Geniius <crescent.sambila@gmail.com>
2023-09-07 09:10:28 +0300

    Booking through email

--- booking.php
+++ booking.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include "controller/dbconfig.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +10,18 @@
 
 <body>
     <div class="container-xxl bg-white p-0">
-    <?php include "includes/header.php"; ?>
+        <?php
+        $hotel_id = $_GET['hotel_id'];
+        $room_id = $_GET['room_id'];
+        $sql = $conn->prepare("SELECT h.name AS hotel_name, rt.name AS type_name FROM rooms r
+        JOIN hotels h ON r.admin_id = h.admin_id
+        JOIN room_types rt ON rt.id = r.type WHERE r.admin_id = ? AND r.id = ?");
+        $sql->bind_param('ss', $hotel_id, $room_id);
+        $sql->execute();
+        $arr = $sql->get_result();
+        $value = $arr->fetch_assoc();
+
+        include "includes/header.php"; ?>
 
 
         <!-- Page Header Start -->
@@ -16,8 +32,8 @@
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb justify-content-center text-uppercase">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                            <li class="breadcrumb-item text-white active" aria-current="page">Booking</li>
+                            <li class="breadcrumb-item"><a href="#"><?php echo $value['hotel_name']; ?></a></li>
+                            <li class="breadcrumb-item text-white active" aria-current="page"><?php echo $value['type_name']; ?></li>
                         </ol>
                     </nav>
                 </div>
@@ -31,97 +47,107 @@
             <div class="container">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
                     <h6 class="section-title text-center text-primary text-uppercase">Room Booking</h6>
-                    <h1 class="mb-5">Book A <span class="text-primary text-uppercase">Luxury Room</span></h1>
+                    <h1 class="mb-5">Book A <span class="text-primary text-uppercase"><?php echo $value['type_name']; ?></span> ROOM</h1>
                 </div>
                 <div class="row g-5">
+                    <?php if (isset($_GET['err'])) {
+                        echo '<div class="col-12">
+                                    <div class="alert alert-danger wow fadeInUp" data-wow-delay="0.3s">' . urldecode(base64_decode($_GET['err'])) . '</div>
+                                </div>';
+                    }
+                    if (isset($_GET['success'])) {
+                        echo '<div class="col-12">
+                                        <div class="alert alert-success wow fadeInUp" data-wow-delay="0.3s">' . urldecode(base64_decode($_GET['success'])) . '</div>
+                                    </div>';
+                    } ?>
                     <div class="col-lg-6">
                         <div class="row g-3">
                             <div class="col-6 text-end">
-                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.1s"
-                                    src="img/about-1.jpg" style="margin-top: 25%;">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.1s" src="img/about-1.jpg" style="margin-top: 25%;">
                             </div>
                             <div class="col-6 text-start">
-                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.3s"
-                                    src="img/about-2.jpg">
+                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.3s" src="img/about-2.jpg">
                             </div>
                             <div class="col-6 text-end">
-                                <img class="img-fluid rounded w-50 wow zoomIn" data-wow-delay="0.5s"
-                                    src="img/about-3.jpg">
+                                <img class="img-fluid rounded w-50 wow zoomIn" data-wow-delay="0.5s" src="img/about-3.jpg">
                             </div>
                             <div class="col-6 text-start">
-                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.7s"
-                                    src="img/about-4.jpg">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.7s" src="img/about-4.jpg">
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="wow fadeInUp" data-wow-delay="0.2s">
-                            <form action="payments.php">
+                            <form action="controller/app.php?action=book_room&hotel_id=<?php echo $hotel_id . '&hotel_name='.$value['hotel_name'].'&room_id=' . $room_id.'&room_name='.$value['type_name']; ?>" method="POST">
                                 <div class="row g-3">
                                     <div class="col-md-6">
                                         <div class="form-floating">
-                                            <input type="text" class="form-control" id="name" placeholder="Your Name">
+                                            <input required type="text" name="name" class="form-control" id="name" placeholder="Your Name">
                                             <label for="name">Your Name</label>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-floating">
-                                            <input type="tel" class="form-control" id="phone" placeholder="Your Phone Number">
+                                            <input type="tel" name="phone" class="form-control" id="phone" placeholder="Your Phone Number">
                                             <label for="phone">Your Phone</label>
                                         </div>
                                     </div>
                                     <div class="col-12">
                                         <div class="form-floating">
-                                            <input type="email" class="form-control" id="email"
-                                                placeholder="Your Email">
+                                            <input required type="email" name="email" class="form-control" id="email" placeholder="Your Email">
                                             <label for="email">Your Email</label>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-floating date" id="date3" data-target-input="nearest">
-                                            <input type="text" class="form-control datetimepicker-input" id="checkin"
-                                                placeholder="Check In" data-target="#date3"
-                                                data-toggle="datetimepicker" />
+                                            <input required type="text" name="checkin" class="form-control datetimepicker-input" id="checkin" placeholder="Check In" data-target="#date3" data-toggle="datetimepicker" />
                                             <label for="checkin">Check In</label>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-floating date" id="date4" data-target-input="nearest">
-                                            <input type="text" class="form-control datetimepicker-input" id="checkout"
-                                                placeholder="Check Out" data-target="#date4"
-                                                data-toggle="datetimepicker" />
+                                            <input required type="text" name="checkout" class="form-control datetimepicker-input" id="checkout" placeholder="Check Out" data-target="#date4" data-toggle="datetimepicker" />
                                             <label for="checkout">Check Out</label>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-md-4">
                                         <div class="form-floating">
-                                            <select class="form-select" id="select1">
-                                                <option value="1">Adult 1</option>
-                                                <option value="2">Adult 2</option>
-                                                <option value="3">Adult 3</option>
+                                            <select required name="adult" class="form-select" id="select1">
+                                                <option value="">Select Adult</option>
+                                                <option value="1">1 Adult</option>
+                                                <option value="2">2 Adults</option>
+                                                <option value="3">3 Adults</option>
+                                                <option value="0">None</option>
                                             </select>
                                             <label for="select1">Select Adult</label>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-md-4">
                                         <div class="form-floating">
-                                            <select class="form-select" id="select2">
-                                                <option value="1">Child 1</option>
-                                                <option value="2">Child 2</option>
-                                                <option value="3">Child 3</option>
+                                            <select required name="child" class="form-select" id="select2">
+                                                <option value="">Select Child</option>
+                                                <option value="1">1 Child</option>
+                                                <option value="2">2 Child</option>
+                                                <option value="3">3 Child</option>
+                                                <option value="0">None</option>
                                             </select>
                                             <label for="select2">Select Child</label>
                                         </div>
                                     </div>
-                                    <div class="col-12">
+                                    <div class="col-md-4">
                                         <div class="form-floating">
-                                            <textarea class="form-control" placeholder="Special Request" id="message"
-                                                style="height: 100px"></textarea>
-                                            <label for="message">Special Request</label>
+                                            <input required type="number" name="rooms_total" class="form-control" id="rooms_total" placeholder="Number of rooms" />
+                                            <label for="rooms_total">Number of Rooms</label>
                                         </div>
                                     </div>
                                     <div class="col-12">
-                                        <button class="btn btn-primary w-100 py-3" type="submit">Book Now</button>
+                                        <div class="form-floating">
+                                            <textarea name="request" class="form-control" placeholder="Special Request" id="request" style="height: 100px"></textarea>
+                                            <label for="request">Special Request</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <button class="btn btn-primary w-100 py-3" name="submit" type="submit">Book Now</button>
                                     </div>
                                 </div>
                             </form>

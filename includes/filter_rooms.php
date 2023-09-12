968798346a558d52699acc0936a51c7243dd604c
Crescent Geniius <crescent.sambila@gmail.com>
2023-09-12 16:03:17 +0300

    Implementing the filter

--- includes/filter_rooms.php
+++ includes/filter_rooms.php
@@ -4,19 +4,19 @@
                 <div class="bg-white shadow" style="padding: 35px;">
                     <div class="row g-2">
                         <div class="col-md-10">
-                            <div class="row g-2">
-                                <div class="col-md-3">
+                            <form class="row g-2">
+                                <div class="col-md-6">
                                     <div class="date" id="date1" data-target-input="nearest">
-                                        <input type="text" class="form-control datetimepicker-input" placeholder="Check in" data-target="#date1" data-toggle="datetimepicker" />
+                                        <input type="text" id="checkin" name="checkin" class="form-control datetimepicker-input" placeholder="Check in" data-target="#date1" data-toggle="datetimepicker" />
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-6">
                                     <div class="date" id="date2" data-target-input="nearest">
-                                        <input type="text" class="form-control datetimepicker-input" placeholder="Check out" data-target="#date2" data-toggle="datetimepicker" />
+                                        <input type="text" id="checkout" name="checkout" class="form-control datetimepicker-input" placeholder="Check out" data-target="#date2" data-toggle="datetimepicker" />
                                     </div>
                                 </div>
-                                <div class="col-md-3">
-                                    <select class="form-select">
+                                <!-- <div class="col-md-3">
+                                    <select id="adults" name="adults" class="form-select">
                                         <option selected>Adult</option>
                                         <option value="1">Adult 1</option>
                                         <option value="2">Adult 2</option>
@@ -24,17 +24,17 @@
                                     </select>
                                 </div>
                                 <div class="col-md-3">
-                                    <select class="form-select">
+                                    <select id="child" name="child" class="form-select">
                                         <option selected>Child</option>
                                         <option value="1">Child 1</option>
                                         <option value="2">Child 2</option>
                                         <option value="3">Child 3</option>
                                     </select>
-                                </div>
-                            </div>
+                                </div> -->
+                            </form>
                         </div>
                         <div class="col-md-2">
-                            <button class="btn btn-primary w-100">Filter</button>
+                            <button id="search-btn" class="btn btn-primary w-100">Filter</button>
                         </div>
                     </div>
                 </div>
